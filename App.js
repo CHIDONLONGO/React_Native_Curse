60fd94f3064f20bb6098dac31afe33b4d0df3e30
CHIDONLONGO <jair.jcp@live.com.mx>
2018-12-27 17:38:14 -0600

    Se agrego el component images

--- App.js
+++ App.js
@@ -2,13 +2,17 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import AppButton from './components/AppButton'
 import PreLoader from './components/PreLoader';
+import BackgraundImage from './components/BackgroundImage';
 
 export default class App extends React.Component {
   render() {
     return (
-      
-       <PreLoader></PreLoader>
-      
+
+      <BackgraundImage
+        source={require('./assets/images/reimi.jpg')}
+      >
+      </BackgraundImage>
+
     );
   }
 }
