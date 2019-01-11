e4ffc2fb9913408683e1ed0ac12b269146b8db71
CHIDONLONGO <jair.jcp@live.com.mx>
2019-01-11 13:16:56 -0600

    Se agrego el componente de searchbox

--- screens/Restaurants/Restaurants.js
+++ screens/Restaurants/Restaurants.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { StyleSheet, FlatList } from 'react-native';
 import BackgroundImage from "../../components/BackgroundImage";
 import PreLoader from '../../components/PreLoader';
-import { ListItem } from "react-native-elements";
+import { ListItem, SearchBar } from "react-native-elements";
 import * as firebase from 'firebase';
 import { NavigationActions } from 'react-navigation';
 import RestaurantEmpty from "../../components/Restaurant/RestaurantEmpty";
@@ -16,14 +16,24 @@ export default class Restaurant extends Component {
     this.state = {
       restaurants: [],
       loaded: false,
-      restaurant_logo: require('../../assets/images/palmas.png')
+      restaurant_logo: require('../../assets/images/palmas.png'),
+      search: ''
     };
-
-    this.refRestaurants = firebase.database().ref().child('restaurants');
-
   }
 
   componentDidMount() {
+    const { search } = this.state;
+
+    if (!search) {
+      this.refRestaurants = firebase.database().ref().child('restaurants');
+    } else {
+      this._filterRestaurants(search);
+    }
+
+    this._loadFirebaseRestaurants();
+  }
+
+  _loadFirebaseRestaurants() {
     this.refRestaurants.on('value', snapshot => {
       let restaurants = [];
       snapshot.forEach(row => {
@@ -64,11 +74,44 @@ export default class Restaurant extends Component {
         containerStyle={styles.item}
         titleStyle={styles.title}
         title={`${restaurant.name}(Capacidad: ${restaurant.capacity})`}
-        leftAvatar={{source:this.state.restaurant_logo}}
+        leftAvatar={{ source: this.state.restaurant_logo }}
         onPress={() => this.restaurantDetail(restaurant)}
         rightIcon={{ name: 'arrow-right', type: 'font-awesome', style: styles.listIconStyle }}
       />
     )
+  }
+
+  searchRestaurants(search) {
+    this.setState({
+      search: search.charAt(0).toUpperCase() + search.slice(1)
+    });
+
+    if (search.length >= 3) {
+      this._filterRestaurants(search);
+      setTimeout(() => {
+        this._loadFirebaseRestaurants();
+      }, 100);
+    }else if (search.length == 0) {
+      this.resetSearch();
+  }
+
+  }
+
+  resetSearch() {
+    this.setState({
+      search: ''
+    });
+    this.refRestaurants = firebase.database().ref().child('restaurants')
+    setTimeout(() => {
+      this._loadFirebaseRestaurants();
+    }, 1000);
+  }
+
+  _filterRestaurants(search) {
+    this.refRestaurants = firebase.database().ref().child('restaurants')
+      .orderByChild('name')
+      .startAt(search)
+      .endAt(`${search}\uf8ff`);
   }
 
   render() {
@@ -78,9 +121,22 @@ export default class Restaurant extends Component {
       return <PreLoader />
     };
 
+    const searchBar = (
+      <SearchBar
+        platform="android"
+        //showLoading
+        cancelIcon={{ type: 'font-awesome', name: 'chevron-left' }}
+        placeholder='Busca algun restaurante!'
+        onChangeText={(text) => this.searchRestaurants(text)}
+        onClear={this.resetSearch.bind(this)}
+        value={this.state.search}
+      />
+    )
+
     if (!restaurants.length) {
       return (
         <BackgroundImage source={require('../../assets/images/reimi.jpg')}>
+        {searchBar}
           <RestaurantEmpty text="No hay restaurantes disponibles" />
           <RestaurantAddButton addRestaurant={this.addRestaurant.bind(this)} />
         </BackgroundImage>
@@ -89,6 +145,9 @@ export default class Restaurant extends Component {
 
     return (
       <BackgroundImage source={require('../../assets/images/reimi.jpg')}>
+
+        {searchBar}
+
         <FlatList
           data={restaurants}
           renderItem={(data) => this.renderRestaurant(data.item)}
